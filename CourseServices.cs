0e6c250b5f7bbe3077f8ab97420afad8940ee2de
Tyler Simmons <tsimmons1@coastal.edu>
2026-02-13 10:37:57 -0500

    Merged with user-story-1/2

--- CourseServices.cs
+++ CourseServices.cs
@@ -43,22 +43,12 @@ namespace cs330_proj1
         //Add more service functions here, as needed, for the project
 
         /* As a student, I want to see all available courses so that I know what my options are */
-
+         public List<Course> getCourses() {
+          return repo.Courses;
+         }
 
         /* As a student, I want to see all course offerings by semester, so that I can choose from what's
            available to register for next semester */
-         public List<CourseOffering> getCourseOfferingsBySemester(String semester) {
-         List<CourseOffering> result = new List<CourseOffering>();
-
-         foreach (CourseOffering co in repo.Offerings) {
-          if (co.Semester.Equals(semester)) {
-            result.Add(co);
-          }
-         }
-
-         return result;
-      }
-
 
         /* As a student I want to see all course offerings by semester and department so that I can 
         choose major courses to register for */
